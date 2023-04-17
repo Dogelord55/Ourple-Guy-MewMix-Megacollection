4abc9350b0bac2d8da789ae5e4f8530ce6c70645
Dogelord55 <dogelord0908@gmail.com>
2023-04-16 20:49:09 -0400

    RPC

--- source/Discord.hx
+++ source/Discord.hx
@@ -17,7 +17,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1097321677903376545",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -45,7 +45,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Vs. Ourple Guy: MewMix-MegaCollection"
 		});
 	}
 
